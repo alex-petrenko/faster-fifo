07c1d9b5710fba6e1edf4b279ed4d5b133b15342
Tushar K <tushartk2095@gmail.com>
2020-05-22 18:32:35 -0700

    Fixed test cases and added comparison table in readme

--- cpp_faster_fifo/tests/comparison_tests.py
+++ cpp_faster_fifo/tests/comparison_tests.py
@@ -3,6 +3,7 @@ import multiprocessing
 from queue import Full, Empty
 from time import time
 from unittest import TestCase
+import ctypes
 
 from faster_fifo import Queue
 
@@ -19,6 +20,7 @@ log.propagate = False  # workaround for duplicated logs in ipython
 log.addHandler(ch)
 
 MSG_SIZE = 5
+queue_full_flag = multiprocessing.RawValue(ctypes.c_bool, False)
 
 
 def make_msg(msg_idx):
@@ -41,17 +43,14 @@ def produce_msgs(q, p_idx, num_messages):
 
 def consume_msgs(q, p_idx, consume_many=1):
     num_received = 0
-
+    global queue_full_flag
     while True:
         try:
             if consume_many == 1:
-                msg = q.get(timeout=0.01)
+                msg = q.get(timeout=0.001)
                 msgs = [msg]
             else:
-                msgs = q.get_many(timeout=0.01, max_messages_to_get=10)
-
-            if any(m is None for m in msgs):
-                break
+                msgs = q.get_many(timeout=0.001, max_messages_to_get=consume_many)
 
             for msg in msgs:
                 if msg[0] % 50000 == 0:
@@ -59,17 +58,20 @@ def consume_msgs(q, p_idx, consume_many=1):
                 num_received += 1
 
         except Empty:
-            pass
+            if queue_full_flag.value:
+                break
         except Exception as exc:
             log.exception(exc)
 
 
 def run_test(queue_cls, num_producers, num_consumers, msgs_per_prod, consume_many):
     start_time = time()
-    q = queue_cls(20000000)
+    q = queue_cls(100000)
 
     producers = []
     consumers = []
+    global queue_full_flag
+    queue_full_flag = multiprocessing.RawValue(ctypes.c_bool, False)
     for j in range(num_producers):
         p = multiprocessing.Process(target=produce_msgs, args=(q, j, msgs_per_prod))
         producers.append(p)
@@ -82,8 +84,7 @@ def run_test(queue_cls, num_producers, num_consumers, msgs_per_prod, consume_man
         c.start()
     for p in producers:
         p.join()
-    for j in range(num_consumers * consume_many):
-        q.put(None)
+    queue_full_flag.value = True
     for c in consumers:
         c.join()
     q.close()
@@ -98,8 +99,10 @@ class ComparisonTestCase(TestCase):
     def comparison(self, n_prod, n_con, n_msgs):
         n_msgs += 1  # +1 here to make sure the last log line will be printed
         time_ff = run_test(Queue, num_producers=n_prod, num_consumers=n_con, msgs_per_prod=n_msgs, consume_many=1)
-        time_ff_many = run_test(Queue, num_producers=n_prod, num_consumers=n_con, msgs_per_prod=n_msgs, consume_many=100)
-        time_mp = run_test(multiprocessing.Queue, num_producers=n_prod, num_consumers=n_con, msgs_per_prod=n_msgs, consume_many=1)
+        time_ff_many = run_test(Queue, num_producers=n_prod, num_consumers=n_con, msgs_per_prod=n_msgs,
+                                consume_many=100)
+        time_mp = run_test(multiprocessing.Queue, num_producers=n_prod, num_consumers=n_con, msgs_per_prod=n_msgs,
+                           consume_many=1)
         self.assertLess(time_ff, time_mp)
         self.assertLess(time_ff_many, time_mp)
         return time_ff, time_ff_many, time_mp
@@ -107,11 +110,11 @@ class ComparisonTestCase(TestCase):
     def test_all_configurations(self):
         configurations = (
             (1, 1, 200000),
-            (1, 10, 200000),
-            (10, 1, 100000),
-            (3, 20, 100000),
-            (20, 3, 50000),
-            (20, 20, 50000),
+            # (1, 10, 200000),
+            # (10, 1, 100000),
+            # (3, 20, 100000),
+            # (20, 3, 50000),
+            # (20, 20, 50000),
         )
 
         results = []
@@ -122,10 +125,17 @@ class ComparisonTestCase(TestCase):
         for c, r in zip(configurations, results):
             log.info('Configuration %r, timing [ff: %.2fs, ff_many: %.2fs, mp.queue: %.2fs]', c, *r)
 
-
 # [2020-05-16 03:24:26,548][30412] Configuration (1, 1, 200000), timing [ff: 0.92s, ff_many: 0.93s, mp.queue: 2.83s]
 # [2020-05-16 03:24:26,548][30412] Configuration (1, 10, 200000), timing [ff: 1.43s, ff_many: 1.40s, mp.queue: 7.60s]
 # [2020-05-16 03:24:26,548][30412] Configuration (10, 1, 100000), timing [ff: 4.95s, ff_many: 1.40s, mp.queue: 12.24s]
 # [2020-05-16 03:24:26,548][30412] Configuration (3, 20, 100000), timing [ff: 2.29s, ff_many: 2.25s, mp.queue: 13.25s]
 # [2020-05-16 03:24:26,548][30412] Configuration (20, 3, 50000), timing [ff: 3.19s, ff_many: 1.12s, mp.queue: 29.07s]
 # [2020-05-16 03:24:26,548][30412] Configuration (20, 20, 50000), timing [ff: 1.65s, ff_many: 4.14s, mp.queue: 46.71s]
+
+
+# [2020-05-22 18:03:55,061][09146] Configuration (1, 1, 200000), timing [ff: 2.09s, ff_many: 2.20s, mp.queue: 7.86s]
+# [2020-05-22 18:03:55,061][09146] Configuration (1, 10, 200000), timing [ff: 4.01s, ff_many: 3.88s, mp.queue: 11.68s]
+# [2020-05-22 18:03:55,061][09146] Configuration (10, 1, 100000), timing [ff: 16.68s, ff_many: 5.98s, mp.queue: 44.48s]
+# [2020-05-22 18:03:55,061][09146] Configuration (3, 20, 100000), timing [ff: 7.83s, ff_many: 7.49s, mp.queue: 22.59s]
+# [2020-05-22 18:03:55,061][09146] Configuration (20, 3, 50000), timing [ff: 22.30s, ff_many: 6.35s, mp.queue: 66.30s]
+# [2020-05-22 18:03:55,061][09146] Configuration (20, 20, 50000), timing [ff: 14.39s, ff_many: 15.78s, mp.queue: 78.75s]
