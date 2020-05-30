5572734bcce1e5b5fa960c420c9c8fd065ea12c6
Tushar K <tushartk2095@gmail.com>
2020-05-29 21:19:22 -0700

    Removed support for Python 3.8 as a test case was failing

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open('README.md', 'r') as fh:
 setup(
     # Information
     name='faster-fifo',
-    version='1.0.3',
+    version='1.0.4',
     url='https://github.com/alex-petrenko/faster-fifo',
     author='Aleksei Petrenko & Tushar Kumar',
     license='MIT',
@@ -29,6 +29,6 @@ setup(
     # Build instructions
     ext_modules=cythonize(extensions),
     setup_requires=['setuptools>=45.2.0', 'cython>=0.29'],
-    python_requires='>=3.6',
+    python_requires='>=3.6, <3.8',
     install_requires=["pip>=19.3"],
 )
